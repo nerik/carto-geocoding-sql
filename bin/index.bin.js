eaa7238cb3f03e50371685c9e3f689265f6f01d1
nerik <erik.escoffier@gmail.com>
2016-07-24 21:16:23 +0200

    code style

--- bin/index.bin.js
+++ bin/index.bin.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 var geocode = require('../');
-process.stdout.write(geocode(process.argv[2]))
+process.stdout.write(geocode(process.argv[2]));
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*eslint no-undef: "error"*/
 
 var prepareFragments = require('./src/prepareFragments');
+var transformFragments = require('./src/transformFragments');
 var SQLFunction = require('./src/SQLFunction');
 var SQL = require('./src/SQL');
 
@@ -9,11 +10,12 @@ module.exports = function(location) {
     throw new Error('no arguments');
   }
   if (typeof location !== 'string') {
-    throw new Error('not a string')
+    throw new Error('not a string');
   }
 
   var fragments = prepareFragments(location);
   var sqlFun = SQLFunction(fragments);
+  fragments = transformFragments(fragments, sqlFun);
   var sql = SQL(fragments, sqlFun);
   return sql;
 }
