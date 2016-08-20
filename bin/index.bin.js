d4b449a1347806ff07b6dedb1d5f355c791338d1
nerik <erik.escoffier@gmail.com>
2016-08-20 17:19:38 +0200

    cli fixes

--- bin/index.bin.js
+++ bin/index.bin.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 var geocode = require('../');
-process.stdout.write(geocode(process.argv[2]));
+
+if (!process.argv[2] || process.argv[2] === '') {
+  process.stderr.write('no location specified\n');
+  process.exit(1);
+}
+
+process.stdout.write(geocode(process.argv[2])+'\n');
